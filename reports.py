b7d37441e88d9044bc5fee4724cf4a3560de9566
Moctar Diallo <moctarjallo@gmail.com>
2022-05-05 21:10:58 +0000

    Finished reports.py

--- reports.py
+++ reports.py
@@ -1,22 +1,21 @@
 #! /usr/bin/env python3
-
-from doctest import REPORT_CDIFF
-from click import style
+from datetime import date
 from reportlab.platypus import SimpleDocTemplate
 from reportlab.platypus import Paragraph, Spacer, Table, Image
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib import colors
 
-def generate(filename, title, additional_info, table_data):
+def generate(filename, title, data):
+    title = title + date.today().strftime("%d-%m-%Y")
     styles = getSampleStyleSheet()
     report = SimpleDocTemplate(filename)
     report_title = Paragraph(title, styles["h1"])
-    report_info = Paragraph(additional_info, styles['BodyText'])
-    table_style = [('GRID', (0, 0), (-1, -1), 1, colors.black),
-                   ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
-                   ('ALIGH', (0, 0), (-1, -1), 'CENTER')]
-    report_table = Table(data=table_data, style=table_style, hAlign='LEFT')
-    empty_line = Spacer(1, 20)
-    report.build([report_title, empty_line, report_info, empty_line, report_table])
+    report_body = [report_title]
+    for item in data:
+        report_text = "name: {} {} weight: {}".format(item['name'], '<br/><br/>', item['weight'])
+        report_data = Paragraph(report_text, styles['BodyText'])
+        empty_line = Spacer(1, 20)
+        report_body.extend([empty_line, report_data])
+    report.build(report_body)
 
 
