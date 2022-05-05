3df636159aefad2f51b90b8be77590338b43827c
Moctar Diallo <moctarjallo@gmail.com>
2022-05-05 22:47:34 +0000

    Fix date format in pdf report title

--- reports.py
+++ reports.py
@@ -7,13 +7,13 @@ from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib import colors
 
 def generate_report(attachement='/tmp/processed.pdf', title="Processed Update on ", data=None):
-    title = title + date.today().strftime("%d-%m-%Y")
+    title = title + date.today().strftime("%b %d, %Y")
     styles = getSampleStyleSheet()
     report = SimpleDocTemplate(attachement)
     report_title = Paragraph(title, styles["h1"])
     report_body = [report_title]
     for item in data:
-        report_text = "name: {} {} weight: {}".format(item['name'], '<br/><br/>', item['weight'])
+        report_text = "name: {} {} weight: {}".format(item['name'], '<br/>', item['weight'])
         report_data = Paragraph(report_text, styles['BodyText'])
         empty_line = Spacer(1, 20)
         report_body.extend([empty_line, report_data])
