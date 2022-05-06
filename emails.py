4b3126c8f4b08c6b6ebe0e56dab2d1d359aa107b
Moctar Diallo <moctarjallo@gmail.com>
2022-05-06 16:27:40 +0000

    Handle case when there is no attachement to the email

--- emails.py
+++ emails.py
@@ -6,7 +6,7 @@ import os.path
 import smtplib
 
 
-def generate_email(sender, recipient, subject, body, attachement_path):
+def generate_email(sender, recipient, subject, body, attachement_path=None):
     """Create an email with an attachement"""
     # Basic email formatting
     message = email.message.EmailMessage()
@@ -16,14 +16,15 @@ def generate_email(sender, recipient, subject, body, attachement_path):
     message.set_content(body)
 
     # Process the attachement and add it to the email
-    attachement_filename = os.path.basename(attachement_path)
-    mime_type, _ = mimetypes.guess_type(attachement_filename)
-    mime_type, mime_subtype = mime_type.split('/', 1)
-    with open(attachement_path, 'rb') as ap:
-        message.add_attachment(ap.read(),
-                               maintype=mime_type,
-                               subtype=mime_subtype,
-                               filename=attachement_filename)
+    if attachement_path:
+        attachement_filename = os.path.basename(attachement_path)
+        mime_type, _ = mimetypes.guess_type(attachement_filename)
+        mime_type, mime_subtype = mime_type.split('/', 1)
+        with open(attachement_path, 'rb') as ap:
+            message.add_attachment(ap.read(),
+                                maintype=mime_type,
+                                subtype=mime_subtype,
+                                filename=attachement_filename)
     return message
 
 def send_email(message):
